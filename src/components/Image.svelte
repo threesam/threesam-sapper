6238f5c8100ebcf185fb83a70451bc1a865ff921
threesam <salvatoredangelo@protonmail.com>
2021-03-24 08:56:39 -0400

    restored Image component to empty, original format

--- src/components/Image.svelte
+++ src/components/Image.svelte
@@ -1,11 +1,6 @@
 <script>
-  import imageBuilder from '../utils/imageUrlBuilder'
-  export let url;
-  export let alt;
-
-  let image
-  let w = image.offsetWidth
-  let h = image.offsetHeight
+  export let url
+  export let alt
 </script>
 
 <style>
@@ -14,4 +9,4 @@
   }
 </style>
 
-<img bind:this={image} src={imageBuilder(url).width(w).height(h)} {alt} loading="lazy"/>
+<img src={url} {alt} loading="lazy"/>
